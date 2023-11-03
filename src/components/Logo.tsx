58353dae957feee25a58c957d7baad593157ae76
Paul <paul.fermey@epitech.eu>
2023-11-03 13:18:35 +0100

    fix(image)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
-import iconicMark from '@/images/iconic-mark.png'
 import Image from 'next/image'
+import iconicMark from '@/images/iconic-mark.png'
 
 export function Logo(props: React.ComponentPropsWithoutRef<any>) {
-  return <Image src={iconicMark} alt="Klaq" {...props} />
+  return <Image width={500} src={iconicMark} alt="Klaq" {...props} />
 }
